20c20e35c84c823a8fd364395372ae8775355e83
EpicBlade <jaydeepmodha1@gmail.com>
2025-02-09 16:52:04 -0500

    ADDED: More weapons and potions in scene

--- Unity_Cashier_VR/Assets/LightHouseData/Scripts/ShieldGrabRotationCorrector.cs
+++ Unity_Cashier_VR/Assets/LightHouseData/Scripts/ShieldGrabRotationCorrector.cs
@@ -1,55 +1,76 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class ShieldGrabRotationCorrector : MonoBehaviour
 {
-    [SerializeField] private InputActionProperty leftHandGripProperty;
-    [SerializeField] private InputActionProperty rightHandGripProperty;
-    [SerializeField] private Transform attachPoint;
+    #region Discarded Code
 
-    private XRGrabInteractable grabInteractable;
-    private bool isLastGrabbedByLeftHand;
+    //[SerializeField] private InputActionProperty leftHandGripProperty;
+    //[SerializeField] private InputActionProperty rightHandGripProperty;
+    //[SerializeField] private Transform attachPoint;
 
-    private void OnEnable()
+    //private XRGrabInteractable grabInteractable;
+    //private bool isLastGrabbedByLeftHand;
+
+    //private void OnEnable()
+    //{
+    //    grabInteractable = GetComponent<XRGrabInteractable>();
+    //    grabInteractable.selectEntered.AddListener(HandleGrab);
+    //    grabInteractable.selectExited.AddListener(HandleLetGo);
+    //}
+
+    //private void OnDisable()
+    //{
+    //    grabInteractable.selectEntered.RemoveListener(HandleGrab);
+    //    grabInteractable.selectExited.RemoveListener(HandleLetGo);
+    //}
+
+    //private void HandleGrab(SelectEnterEventArgs args)
+    //{
+    //    var currentRot = attachPoint.localRotation;
+    //    var newRot = Quaternion.identity;
+
+    //    if (leftHandGripProperty.action.IsPressed() && !isLastGrabbedByLeftHand)
+    //    {
+    //        newRot = Quaternion.Euler(currentRot.x, 90f, currentRot.z);
+    //        isLastGrabbedByLeftHand = true;
+    //    }
+    //    else if (rightHandGripProperty.action.IsPressed())
+    //    {
+    //        newRot = Quaternion.Euler(currentRot.x, 270f, currentRot.z);
+    //        isLastGrabbedByLeftHand = false;
+    //    }
+
+    //    attachPoint.localRotation = newRot;
+    //    Debug.Log("Grab: " + isLastGrabbedByLeftHand);
+    //}
+
+    //private void HandleLetGo(SelectExitEventArgs args)
+    //{
+    //    if (!leftHandGripProperty.action.IsPressed() && !rightHandGripProperty.action.IsPressed())
+    //        isLastGrabbedByLeftHand = false;
+    //    Debug.Log("Let Go: " + isLastGrabbedByLeftHand);
+    //}
+    #endregion
+
+    [SerializeField] private List<Collider> collidersToDisable;
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        grabInteractable = GetComponent<XRGrabInteractable>();
-        grabInteractable.selectEntered.AddListener(HandleGrab);
-        grabInteractable.selectExited.AddListener(HandleLetGo);
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void OnDisable()
+    public void OnSelectEntered(SelectEnterEventArgs args)
     {
-        grabInteractable.selectEntered.RemoveListener(HandleGrab);
-        grabInteractable.selectExited.RemoveListener(HandleLetGo);
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.None;
     }
 
-    private void HandleGrab(SelectEnterEventArgs args)
+    public void OnSelectExited(SelectExitEventArgs args)
     {
-        var currentRot = attachPoint.localRotation;
-        var newRot = Quaternion.identity;
-
-        if (leftHandGripProperty.action.IsPressed() && !isLastGrabbedByLeftHand)
-        {
-            newRot = Quaternion.Euler(currentRot.x, 90f, currentRot.z);
-            isLastGrabbedByLeftHand = true;
-        }
-        else if (rightHandGripProperty.action.IsPressed())
-        {
-            newRot = Quaternion.Euler(currentRot.x, 270f, currentRot.z);
-            isLastGrabbedByLeftHand = false;
-        }
-
-        attachPoint.localRotation = newRot;
-        Debug.Log("Grab: " + isLastGrabbedByLeftHand);
-    }
-
-    private void HandleLetGo(SelectExitEventArgs args)
-    {
-        if (!leftHandGripProperty.action.IsPressed() && !rightHandGripProperty.action.IsPressed())
-            isLastGrabbedByLeftHand = false;
-        Debug.Log("Let Go: " + isLastGrabbedByLeftHand);
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.None;
     }
 }
